6c24b6ea6215339f2f3b6bd2e7b304850aab8973
Jeanpro2004 <jean.rodriguez.ortiz@udla.edu.ec>
2025-06-25 15:04:20 -0500

    Implementacion de Modelo de Miembros de grupo

---
+++ AppApuntes2CRR/Models/TeamMember.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerGrupalMMVC.Models
+{
+    public class TeamMember
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string FavoriteSport { get; set; }
+        public string PhotoPath { get; set; }
+        public string Role { get; set; }
+        public string Description { get; set; }
+    }
+}
