b018e62c09220e2ebb29b843ce6d961e651ce223
Jeanpro2004 <jean.rodriguez.ortiz@udla.edu.ec>
2025-06-25 14:45:51 -0500

    Implementacion de la clase Modelos y su contenido de Modelo Recordatorio

---
+++ AppApuntes2CRR/Models/Recordatorio.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerGrupalMMVC.Models
+{
+    public class Recordatorio
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Texto { get; set; } = "";
+        public DateTime FechaHora { get; set; } = DateTime.Now;
+        public bool Activo { get; set; } = true;
+
+        
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+
+        
+        public string FechaHoraFormateada => FechaHora.ToString("dd/MM/yyyy HH:mm");
+        public string TiempoRestante
+        {
+            get
+            {
+                var diferencia = FechaHora - DateTime.Now;
+                if (diferencia.TotalMinutes < 0)
+                    return "Vencido";
+                else if (diferencia.TotalHours < 24)
+                    return $"En {diferencia.Hours}h {diferencia.Minutes}m";
+                else
+                    return $"En {diferencia.Days} días";
+            }
+        }
+    }
+}
